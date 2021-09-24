338369d4067649ea626a33610221debd9faf2a68
Dominik Labudzinski <dominik.labudzinski@brandoriented.pl>
2021-09-24 10:07:59 +0200

    UrlUtils

--- src/main/java/com/lemric/utils/URLUtils.java
+++ src/main/java/com/lemric/utils/URLUtils.java
@@ -19,6 +19,8 @@ package com.lemric.utils;
  */
 
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,12 +31,108 @@ public class URLUtils {
     private static Pattern p = null;
     private static String lastKey = null;
 
-    public static String rawUrlDecode(String url) {
-        return java.net.URLDecoder.decode(url, StandardCharsets.UTF_8);
+    public static String rawUrlDecode(String s)
+    {
+        if (s == null)
+            return "";
+
+        int len = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '%' && i + 2 < len) {
+                int d1 = s.charAt(i + 1);
+                int d2 = s.charAt(i + 2);
+
+                int v = 0;
+
+                if ('0' <= d1 && d1 <= '9')
+                    v = 16 * (d1 - '0');
+                else if ('a' <= d1 && d1 <= 'f')
+                    v = 16 * (d1 - 'a' + 10);
+                else if ('A' <= d1 && d1 <= 'F')
+                    v = 16 * (d1 - 'A' + 10);
+                else {
+                    sb.append('%');
+                    continue;
+                }
+
+                if ('0' <= d2 && d2 <= '9')
+                    v += (d2 - '0');
+                else if ('a' <= d2 && d2 <= 'f')
+                    v += (d2 - 'a' + 10);
+                else if ('A' <= d2 && d2 <= 'F')
+                    v += (d2 - 'A' + 10);
+                else {
+                    sb.append('%');
+                    continue;
+                }
+
+                i += 2;
+                sb.append((char) v);
+            }
+            else
+                sb.append(ch);
+        }
+
+        return sb.toString();
     }
-    public static String rawUrlEncode(String url) {
-        return java.net.URLEncoder.encode(url, StandardCharsets.UTF_8);
+    public static String rawUrlEncode(String str) {
+        if (str == null) {
+            return "";
+        }
+
+        str = str.replace("\\\\", "\\");
+        Charset charset;
+        charset = StandardCharsets.UTF_8;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if ('a' <= ch && ch <= 'z'
+                    || 'A' <= ch && ch <= 'Z'
+                    || '0' <= ch && ch <= '9'
+                    || ch == '-' || ch == '_' || ch == '.' || ch == '~') {
+                sb.append(ch);
+            }
+            else {
+                if(ch>128){
+                    byte[] ba = (ch+ "").getBytes(charset);
+                    for (byte b : ba) {
+                        sb.append('%');
+                        char ch0 = Character.forDigit((b >> 4) & 0xF, 16);
+
+                        if (Character.isLetter(ch0)) {
+                            ch0 -= caseDiff;
+                        }
+                        sb.append(ch0);
+                        ch0 = Character.forDigit(b & 0xF, 16);
+                        if (Character.isLetter(ch0)) {
+                            ch0 -= caseDiff;
+                        }
+                        sb.append(ch0);
+                    }
+                } else {
+                    sb.append("%");
+                    sb.append(toHexDigit(ch >> 4));
+                    sb.append(toHexDigit(ch));
+                }
+            }
+        }
+
+        return sb.toString();
     }
+
+    private static char toHexDigit(int d) {
+        d = d & 0xf;
+        if (d < 10) {
+            return (char) ('0' + d);
+        } else {
+            return (char) ('A' + d - 10);
+        }
+    }
+    static final int caseDiff = ('a' - 'A');
 
     public static String evaluate(String urlStr, String partToExtract) {
         if (urlStr == null || partToExtract == null) {
